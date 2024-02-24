b6a5799c9a0e940ff278c2367ec3b5625832bd72
Rageshwaran-HR <118850069+Rageshwaran-HR@users.noreply.github.com>
2024-02-24 10:13:49 +0000

    fixed errors

--- learnative/components/navbar.tsx
+++ learnative/components/navbar.tsx
@@ -1,17 +1,18 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
-import {Menu} from "lucide-react";
+import { Menu } from "lucide-react";
 
 const NavBar = () => {
     return (
         <div className="flex items-center p-4">
             <Button variant="ghost" size="icon"
-            className="md:hidden">
-                <Menu/>
+                className="md:hidden">
+                <Menu />
             </Button>
             <div className="flex w-full justify-end">
-                <UserButton afterSignOutUrl ="/"/>
+                <UserButton afterSignOutUrl="/" />
             </div>
         </div>
     );
 }
- export default NavBar;
+export default NavBar;
--- learnative/components/sidebar.tsx
+++ learnative/components/sidebar.tsx
@@ -1,13 +1,21 @@
-"use client";
+// Importing the correct types from "next"
+import Image from 'next/image';
+import Link from 'next/link';
+
+// Call the Montserrat function to load the font
 
 const Sidebar = () => {
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-                   <div className="relative w-8 h-8 mr-4">
-                    <Image fill alt="Logo" src="Logo.png"/>
+                    <div className="relative w-8 h-8 mr-4">
+                        {/* You might need to adjust the Image component based on your actual usage */}
+                        <Image fill alt="Logo" src="/logo.png" />
                     </div>
+                    <h1 className="text-2xl font-bold">
+                        Genius
+                    </h1>
                 </Link>
             </div>
         </div>
