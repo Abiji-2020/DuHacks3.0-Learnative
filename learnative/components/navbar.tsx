62c82477fce2c2fc1e4aadd545c778279e2c5790
Fahrudeen <145411048+Fahrudeen@users.noreply.github.com>
2024-02-24 15:53:24 +0530

    Update navbar.tsx

--- learnative/components/navbar.tsx
+++ learnative/components/navbar.tsx
@@ -1,13 +1,11 @@
 import { Button } from "./ui/button";
 import {Menu} from "lucide-react";
+import MobileSidebar from "@/components/mobile-sidebar";
 
 const NavBar = () => {
     return (
         <div className="flex items-center p-4">
-            <Button variant="ghost" size="icon"
-            className="md:hidden">
-                <Menu/>
-            </Button>
+            <MobileSidebar/>
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl ="/"/>
             </div>
