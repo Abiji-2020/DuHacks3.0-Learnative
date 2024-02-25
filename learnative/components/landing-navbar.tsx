a36c3de3b08f8e7e9ddf665f6c50f05ace3afd3c
Fahrudeen <145411048+Fahrudeen@users.noreply.github.com>
2024-02-25 12:34:13 +0530

    Update landing-navbar.tsx

--- learnative/components/landing-navbar.tsx
+++ learnative/components/landing-navbar.tsx
@@ -18,6 +18,20 @@ export const LandingNavbar = () => {
 
 return(
   <nav className="p-4 bg-transparent flex items-center justify-between">
+    <Link href="/" className="flex items-center">
+      <div className="relative h-8 w-8 mr-4">
+        <Image fill alt="logo" src="/logo.png"/>
+      </div>
+      <h1 className={cn("text-2xl font-bold text-white", font.className)}>
+      Genius
+      </h1>
+    </Link>
+    <div className="flex items-center gap-x-2">
+    <Link href={isSignedIn > "/dashboard" : "/sign-up"}>
+      <Button variant="outline" className="rounded-full">
+        Get Started
+      </Button>
+    </div>
   </nav>
   )
 }
